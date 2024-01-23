7fcf40add179d0e447b65075b9a1b1c152e5c69e
马恒 <heng.ma@mail.utoronto.ca>
2024-01-23 00:02:29 +0000

    add input/out/scripts files

---
+++ scripts/02-data_cleaning.R
@@ -0,0 +1,19 @@
+library(knitr)
+library(janitor)
+library(lubridate)
+library(opendatatoronto)
+library(tidyverse)
+
+
+
+toronto_shelters_clean <-
+  clean_names(toronto_shelters) |>
+  mutate(occupancy_date = ymd(occupancy_date)) |> 
+  select(occupancy_date, occupied_beds)
+
+head(toronto_shelters_clean)
+
+write_csv(
+  x = toronto_shelters_clean,
+  file = "cleaned_toronto_shelters.csv"
+)
