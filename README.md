dacb55d0d299c1753d0e120650aba9e832bd13a9
马恒 <heng.ma@mail.utoronto.ca>
2024-01-24 22:20:53 +0000

    organized

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# STA302Paper1
+
+# Starter folder
+
+## Overview
+
+This paper, by further collecting data from the Open Data Portal of the City of Toronto, aims to analyze the problem and trends by visualizing data related to the least violent profession, police, and its association with violence.
+
+## File Structure
+
+The repo is structured as:
+
+-   `input/data` contains the data sources used in analysis including the raw data.
+-   `outputs/data` contains the cleaned dataset that was constructed.
+-   `outputs/paper` contains the files used to generate the paper, including the Quarto document and reference bibliography file, as well as the PDF of the paper. 
+-   `scripts` contains the R scripts used to simulate, download and clean data.
--- scripts/02-data_cleaning.R
+++ scripts/02-data_cleaning.R
@@ -3,17 +3,24 @@ library(janitor)
 library(lubridate)
 library(opendatatoronto)
 library(tidyverse)
-
-
 library(dplyr)
+library(readr)
 
+data <- read_csv("/cloud/project/toronto_gender.csv")
 
-data <- read.csv('toronto_gender.csv')
-data_selected <- select(data, Perceived_Race_of_People_Involv, Incident_Count)
+data_sorted <- data[order(-data$Incident_Count), ]
+top_10 <- head(data_sorted, 10)
+
+data_selected <- select(top_10, Perceived_Race_of_People_Involv, Incident_Count)
 data_selected$Incident_Count[is.na(data_selected$Incident_Count)] <- 0
 data_reduced <- head(data_selected, 10)
 
 
-print(data_reduced)
-write.csv(data_reduced, 'clean_reduced.csv', row.names = FALSE)
-cat("The data_reduced has been saved to 'data_reduced.csv'")
+data_grouped <- data %>%
+  group_by(Perceived_Race_of_People_Involv) %>%
+  summarise(Incident_Count = sum(Incident_Count, na.rm = TRUE))
+
+print(data_grouped)
+
+write.csv(data_grouped, 'clean_reduced.csv', row.names = FALSE)
+
